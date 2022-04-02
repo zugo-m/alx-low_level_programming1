ce926f108a9fb5d6f8865e5b550e4dea407ca83f
Marvis <marvelscott19@gmail.com>
2022-04-02 19:50:27 +0000

    Task 1 is a function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copiees memory area
+ * @dest: where size of memory area is copied to
+ * @src: where size of memory area is copeid from
+ * @n: size of memory area to copy
+ *
+ * Return : a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	
+	return (dest);
+}
+
