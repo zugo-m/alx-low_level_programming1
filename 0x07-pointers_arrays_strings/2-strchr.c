37643fbb6b7913be65b8a0cc6b834b466a97a8df
Marvis <marvelscott19@gmail.com>
2022-04-02 20:16:49 +0000

    Task 2 is a unction that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string  
+ * @c: characters to locate
+ *
+ * Return : a pointer to the first character occurence or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i++)
+	{
+		if (s[i] = c)
+		{	
+			return (s);
+
+		}
+		if (s[i] == 0)
+		{	
+			return (NULL);
+		}
+	}
+}
